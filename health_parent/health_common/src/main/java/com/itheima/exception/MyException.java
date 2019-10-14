57adfc57bb5fcf3547626cab0f5608a7822212eb
Administrator <18372568672@139.com>
2019-10-14 13:08:04 +0800

    第二次

--- health_parent/health_common/src/main/java/com/itheima/exception/MyException.java
+++
@@ -1,9 +0,0 @@
-package com.itheima.exception;
-
-public class MyException extends RuntimeException{
-
-    //构造函数
-    public MyException(String message) {
-        super(message);
-    }
-}
--- health_parent/health_dao/src/main/java/com/itheima/dao/UserDao.java
+++
@@ -1,9 +0,0 @@
-package com.itheima.dao;
-
-import com.itheima.pojo.User;
-import org.apache.ibatis.annotations.Select;
-
-public interface UserDao {
-
-    User findUserByUsername(String username);
-}
